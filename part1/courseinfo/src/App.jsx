511d760fb30c415ef3d69974454a53f35fe2fa86
zane o <zaneolds@gmail.com>
2024-04-20 15:56:43 -0500

    1.5 refactor so course is one object with name and parts attributes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,28 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14 
-    }
-  ]
+  const course =
+  {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} /> 
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
 
     </div>
   )
@@ -40,9 +43,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part name={props.parts[0].name} number={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} number={props.parts[1].exercises}/>
-      <Part name={props.parts[1].name} number={props.parts[2].exercises}/>
+      <Part name={props.parts[0].name} number={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} number={props.parts[1].exercises} />
+      <Part name={props.parts[1].name} number={props.parts[2].exercises} />
     </>
   )
 }
