103cc1257fa7edfc636dd29d6d243abaada248dc
zane o <zaneolds@gmail.com>
2024-04-20 14:55:04 -0500

    1.2 refactor to include part components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -26,12 +26,18 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.number}</p>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part name={props.part1} number={props.exercises1}/>
+      <Part name={props.part2} number={props.exercises2}/>
+      <Part name={props.part3} number={props.exercises3}/>
     </>
   )
 }
