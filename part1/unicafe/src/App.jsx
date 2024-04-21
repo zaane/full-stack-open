b6d95baa87ff048e3723e53204d6024d331a9eed
zane o <zaneolds@gmail.com>
2024-04-21 17:40:11 -0500

    refactor statistics list into its own component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,6 +11,22 @@ const StatDisplay = (props) => (
   <p>{props.name} {props.value}</p>
 )
 
+const Statistics = (props) => {
+      const good = props.good
+      const neutral = props.neutral
+      const bad = props.bad
+      const total = props.total
+
+      return <>
+        <StatDisplay name={"good"} value={good}/>
+        <StatDisplay name={"neutral"} value={neutral}/>
+        <StatDisplay name={"bad"} value={bad}/>
+        <StatDisplay name={"all"} value={total}/>
+        <StatDisplay name={"average"} value={total ? Math.round(100 * (good - bad)/total) / 100: ' '} />
+        <StatDisplay name={"percent positive"} value={total ? `${Math.round(100*good/total)/100}%` : ' '}/>
+      </>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -20,22 +36,17 @@ const App = () => {
   const total = good + neutral + bad;
 
   const incrementStat = (stat, setStat) => () => setStat(stat + 1)
-  
+
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={incrementStat(good, setGood)} text={'good'}/>
-      <Button handleClick={incrementStat(neutral, setNeutral)} text={'neutral'}/>
-      <Button handleClick={incrementStat(bad, setBad)} text={'bad'}/>
+      <Button handleClick={incrementStat(good, setGood)} text={'good'} />
+      <Button handleClick={incrementStat(neutral, setNeutral)} text={'neutral'} />
+      <Button handleClick={incrementStat(bad, setBad)} text={'bad'} />
 
       <h1>statistics</h1>
-      <StatDisplay name={"good"} value={good}/>
-      <StatDisplay name={"neutral"} value={neutral}/>
-      <StatDisplay name={"bad"} value={bad}/>
-      <StatDisplay name={"all"} value={total}/>
-      <StatDisplay name={"average"} value={total ? Math.round(100 * (good - bad)/total) / 100: ' '} />
-      <StatDisplay name={"percent positive"} value={total ? `${Math.round(100*good/total)/100}%` : ' '}/>
-      
+      <Statistics good={good} neutral= {neutral} bad={bad} total={total} />
+
     </div>
   )
 }
