4b250dba42716e700cf3d21e95db42fbc20a5f52
zane o <zaneolds@gmail.com>
2024-04-23 10:03:50 -0500

    2.6 implement contact adding functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,30 +4,46 @@ const Contact = (props) => {
   return <div>{props.name}</div>
 }
 
-
-
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' },
     { name: 'Barto Bellas' },
     { name: 'Darto Dellas' },
     { name: 'Farto Fellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button
+            onClick={addContact}
+            type="submit">
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Contact name={person.name} />)}
+      {persons.map(person => <Contact key={person.name} name={person.name} />)}
     </div>
   )
 }
