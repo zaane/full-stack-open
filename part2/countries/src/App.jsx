c8286122e5f7017cca41eeed5b39c555c32ed7a6
zane o <zaneolds@gmail.com>
2024-04-25 14:54:31 -0500

    get country names as array

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 const SearchBox = ({ query, onChange }) => {
@@ -10,8 +11,26 @@ const SearchBox = ({ query, onChange }) => {
   </div>
 }
 
+const Result = () => {
+  return (
+    <>
+    result
+    </>
+  )
+}
+
 function App() {
+  const [countries, setCountries] = useState('')
   const [query, setQuery] = useState('')
+
+  const getAllCountryNames = (baseUrl) => {
+    const request = axios.get(baseUrl)
+    return request.then(response => {
+      console.log(response.data.map(datum => datum.name.common))})
+  }
+
+  getAllCountryNames('https://studies.cs.helsinki.fi/restcountries/api/all')
+
 
   const handleSearchChange = (event) => {
     setQuery(event.target.value)
