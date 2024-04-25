3d926f7ab86490ba18db5c53b839075e221cdb7f
zane o <zaneolds@gmail.com>
2024-04-25 14:59:34 -0500

    get country data by name

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -24,12 +24,22 @@ function App() {
   const [query, setQuery] = useState('')
 
   const getAllCountryNames = (baseUrl) => {
-    const request = axios.get(baseUrl)
+    const request = axios.get(`${baseUrl}/all`)
     return request.then(response => {
       console.log(response.data.map(datum => datum.name.common))})
   }
 
-  getAllCountryNames('https://studies.cs.helsinki.fi/restcountries/api/all')
+  getAllCountryNames('https://studies.cs.helsinki.fi/restcountries/api')
+
+  const getCountryByName = (baseUrl, name) => {
+    const request = axios.get(`${baseUrl}/name/${name}`)
+    return request.then(response => {
+      console.log(response.data)
+    })
+  }
+
+  getCountryByName('https://studies.cs.helsinki.fi/restcountries/api', 'kuwait')
+
 
 
   const handleSearchChange = (event) => {
