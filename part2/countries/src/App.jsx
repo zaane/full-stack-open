05166771dd4c1b00a2c4e0ea1a0b8c1a25f9e5fb
zane o <zaneolds@gmail.com>
2024-04-26 14:51:25 -0500

    display country info in countryinfo component

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -22,6 +22,15 @@ const SearchResult = ({ countryName }) => {
 const CountryInfo = ({ country }) => {
   return (
     <div>
+      <h2>{country.name.common}</h2>
+      <div>capital {country.capital}</div>
+      <div>area {country.area} km^2</div>
+
+      <h3>languages:</h3>
+      <ul>
+        {Object.values(country.languages).map(language => <li>{language}</li>)}
+      </ul>
+      <img src={country.flags.png} />
       {console.log(country)}
     </div>
   )
