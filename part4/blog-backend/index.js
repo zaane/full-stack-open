491f5e0520b904c7978088149c1ca44c9b9a20d2
zane o <zaneolds@gmail.com>
2024-05-01 14:52:26 -0500

    4.1 set up server for simple post and get requests

---
+++ part4/blog-backend/index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+console.log(process.env) //TODO: remove
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+morgan.token('content', request => JSON.stringify(request.body))
+
+const requestLogger = morgan((tokens, req, res) => {
+    const format = [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms'
+    ]
+
+    if (req.method === 'POST') format.push(tokens['content'](req, res))
+    return format.join(' ')
+})
+
+app.use(requestLogger)
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    console.log(request.body);
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
