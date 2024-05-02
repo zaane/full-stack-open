6f727c16d73d002ba5904248845ed04c191157b1
zane o <zaneolds@gmail.com>
2024-05-02 12:06:23 -0500

    4.4 test totalLikes helper function

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs
+        .map(blog => blog.likes)
+        .reduce((sum, item) => sum + item, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
