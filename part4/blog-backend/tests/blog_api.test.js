ef2af2b5be3fe04bd36bb49969bfe8ceb20bfc6d
zane o <zaneolds@gmail.com>
2024-05-03 12:42:22 -0500

    add sample blogs to api test

--- part4/blog-backend/tests/blog_api.test.js
+++ part4/blog-backend/tests/blog_api.test.js
@@ -1,11 +1,23 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const blogSamples = require('../blog_samples')
+const initialBlogs = blogSamples.listWithManyBlogs
+
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
