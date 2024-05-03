1d8e8eda83bcaa5df41fbddd61074cb0bf87496c
zane o <zaneolds@gmail.com>
2024-05-03 13:04:19 -0500

    4.8 refactor get requests to use async/await

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -1,18 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
 blogsRouter.post('/', (request, response) => {
-    console.log(request.body);
-    const blog = new Blog(request.body)
+    // console.log(request.body);
+    // const blog = new Blog(request.body)
 
+    // const savedBlog = await blog.save()
+    // response.status(201).json(savedBlog)
     blog
         .save()
         .then(result => {
--- part4/blog-backend/tests/blog_api.test.js
+++ part4/blog-backend/tests/blog_api.test.js
@@ -1,13 +1,13 @@
 const { test, after, beforeEach } = require('node:test')
-const Blog = require('../models/blog')
-const blogSamples = require('../blog_samples')
-const initialBlogs = blogSamples.listWithManyBlogs
-
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+
+const blogSamples = require('../blog_samples')
+const initialBlogs = blogSamples.listWithManyBlogs
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -15,15 +15,21 @@ beforeEach(async () => {
     await blogObject.save()
     blogObject = new Blog(initialBlogs[1])
     await blogObject.save()
-  })
+})
 
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of notes returned', async () => {
+    const response = await api.get('/api/blogs')
+    
+    assert.strictEqual(response.body.length, 2)
 })
 
 after(async () => {
-  await mongoose.connection.close()
+    await mongoose.connection.close()
 })
