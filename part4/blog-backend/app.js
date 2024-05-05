dd18de80ebb5db49e7e4a4274f8e167f3f7af192
zane o <zaneolds@gmail.com>
2024-05-05 12:59:06 -0500

    4.18 implement token-based authentication

--- part4/blog-backend/app.js
+++ part4/blog-backend/app.js
@@ -5,6 +5,7 @@ require('express-async-errors')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
@@ -28,6 +29,7 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
